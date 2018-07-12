d206cb30c819ea3eb283d2a30695d0d51995ff52
projudah <projudah@gmail.com>
2018-07-12 16:50:40 -0400

    first successful run

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 import os
 
-pathmap={}
-traversed=[]
+pathmap = {}
+traversed = []
+
+
 def main():
-	start = "C:/Users/proju/Pictures"
+	start = "C:\\Users\\proju\\Desktop\\classes"
+	print(traverse(3,0,start))
+	print(traversed)
 
 
 '''for aestethic reasons, i'm implementing a breadth first search'''
-def traverse(depth,step,start):
+def traverse(depth, step, start):
+	os.chdir(start)
+	submap={}
+	traversed.append(start)
 	for file in os.listdir(start):
 		current = os.path.abspath(file)
-		if step<depth:
-			traverse(depth,step+1,current)
-		else:
-			pathmap[]
-
+		# current = current.replace(os.sep,'/')
+		if step < depth:
+			if current in traversed:
+				pass
+			else:
+				if os.path.isdir(current):
+					submap[file]=traverse(depth, step + 1, current)
+					os.chdir(start)
+				else:
+					submap[file]={}
+	return submap
 
 if __name__ == '__main__':
 	main()
